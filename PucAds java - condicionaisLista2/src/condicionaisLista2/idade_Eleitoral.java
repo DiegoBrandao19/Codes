6229902c526b28332f73cb15c0da5689e7a02d7c
DiegoBrandao19 <127473528+DiegoBrandao19@users.noreply.github.com>
2023-08-03 15:12:12 -0300

    Update idade_Eleitoral.java

--- PucAds java - condicionaisLista2/src/condicionaisLista2/idade_Eleitoral.java
+++ PucAds java - condicionaisLista2/src/condicionaisLista2/idade_Eleitoral.java
@@ -6,9 +6,19 @@ public class idade_Eleitoral {
 		Scanner teclado = new Scanner (System.in);
 		System.out.println("Digite sua idade:");
 		int idade = teclado.nextInt();
-		
-		
-
+		if(idade<16)
+		 {
+                    System.out.print("Nao tem idade para votar");
+		 }
+		else
+		     if(idade>16 || idade<60)
+		     {
+			System.out.print("Idade obrigatoria a votar");     
+		     }
+		else
+		     {
+			System.out.print("Nao e obrigatorio votar");
+		     }
 	}
 
 }
